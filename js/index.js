c1c0d87eba489dc2a54802e93072872c9ab2c337
Kay Kemmett <263657797+KayK12@users.noreply.github.com>
2026-06-02 13:47:42 -0400

    API fetch completed

--- js/index.js
+++ js/index.js
@@ -25,3 +25,21 @@ for(let i = 0; i < skills.length; i++){
     skill.innerText = skills[i];
     skillsList.appendChild(skill);
 };
+
+const projectSection = document.querySelector("#Projects")
+const projectList = projectSection.querySelector("ul")
+
+fetch(`https://api.github.com/users/KayK12/repos`)
+    .then(response => response.json())
+    .then(repositories => {
+        console.log(repositories);
+
+        for (let i = 0; i < repositories.length; i++){
+            let project = document.createElement("li");
+            project.innerText = repositories[i].name;
+            projectList.appendChild(project);
+}
+    })
+    .catch(error => {
+        console.log("No repositories found.");
+    });
